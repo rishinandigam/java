97ac2cee4c1822288c908ee37e3f7b385493a885
rishinandigam <gnyanarushi@gamil.com>
2024-05-27 15:27:17 +0530

    abstract classes

--- Demo1.java
+++ Demo1.java
@@ -1,19 +1,19 @@
 class test {
-    void display()
-    {
-       System.out.println("hello 1");
+    void display() {
+        System.out.println("hello 1");
     }
 }
+
 public class Demo1 extends test {
-   
-    
-    void display()
-    {
+
+    void display() {
+        super.display();
         System.out.println("Hello 2 ");
     }
+
     public static void main(String[] args) {
-       Demo1 d1 = new Demo1();
-       d1.display();
-    
+        Demo1 d1 = new Demo1();
+        d1.display();
+
     }
 }
