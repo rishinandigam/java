cf01d01fd588eb319f23eaa29f67c43ab4b9fd86
rishinandigam <gnyanarushi@gamil.com>
2024-05-28 10:13:58 +0530

    java

---
+++ abstractMain.java
@@ -0,0 +1,20 @@
+abstract class A {
+     abstract void display();
+}
+class B extends A
+{
+    
+  
+    void display() {
+        {
+            System.out.println("this is an abstract class .\n");
+        }
+    }
+}
+class abstractMain
+{
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+    }
+}
