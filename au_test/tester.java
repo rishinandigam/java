75ed4a519f8e0593c1ed24fd8721c1009106f6b8
rishinandigam <gnyanarushi@gmail.com>
2024-06-02 13:52:26 +0530

    .in

--- au_test/tester.java
+++ au_test/tester.java
@@ -1,20 +1,22 @@
 package au_test;
+
 import automobile.*;
+
 @SuppressWarnings("unused")
 public class tester {
-public static void main(String s[ ] ) {
-System.out.println(" I am tester class defined inside au_tester package");
-System.out.println(" I had imported all classes of automobile package");
-System.out.println(" Creating instances of Vehicle, Car and Bike ");
-System.out.println(" ------------------------");
-Vehicle v = new Vehicle();
-Car c = new Car();
-Bike b = new Bike();
-System.out.println(" Accessing the functions using objects");
-System.out.println(" --------------------- ");
- v.printname();
- c.printname();
- b.printname();
+    public static void main(String s[]) {
+        System.out.println(" I am tester class defined inside au_tester package");
+        System.out.println(" I had imported all classes of automobile package");
+        System.out.println(" Creating instances of Vehicle, Car and Bike ");
+        System.out.println(" ------------------------");
+        Vehicle v = new Vehicle();
+        Car c = new Car();
+        Bike b = new Bike();
+        System.out.println(" Accessing the functions using objects");
+        System.out.println(" --------------------- ");
+        v.printname();
+        c.printname();
+        b.printname();
 
-}
+    }
 }
---
+++ firstInterface.java
@@ -0,0 +1,8 @@
+public interface firstInterface {
+
+     int addMethod(int x , int y);
+     float divMethod(float a , float b );
+     void display();
+     int var1 =10;
+      double var2 =20.6 ;
+}
