2e813b56ac7235747460c8a737dd0123eb7f48f3
rishinandigam <gnyanarushi@gmail.com>
2024-06-02 14:40:09 +0530

    interface

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+
+    class MyClass implements firstInterface{
+        public int  addMethod(int x, int y){
+            return(x+y);
+        }
+    
+        public float divMethod(float a, float b){
+            return(a/b);
+        }
+    
+        public void display(){
+            System.out.println("Variable 1 :" +var1);
+            System.out.println("Variable 2 :" +var2);
+        }
+    }
+    
+
